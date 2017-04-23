b343f85b0dd968fa8c16df9eb08223fdee8e895d
hjl <799728970@qq.com>
2017-04-23 20:32:24 +0800

    添加接口

--- src/main/java/com/hjl/controller/Index.java
+++ src/main/java/com/hjl/controller/Index.java
@@ -3,7 +3,9 @@ package com.hjl.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Index
@@ -19,10 +21,17 @@ public class Index {
 	@RequestMapping("redirect")
 	public String test(HttpServletResponse response){
 		
-		return "redirect:http://baidu.com";
+		return "redirect:123.57.238.125/session-test/login?code=123&state=456";
 	}
 	
 	
+	@RequestMapping("login")
+	public String login(HttpServletRequest request ,String code , String state){
+		HttpSession session = request.getSession();
+		session.setAttribute("code" ,code);
+		session.setAttribute("state" , state);
+		return "redirect:index";
+	}
 	
 	
 	public static void print(Object o) {
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -11,6 +11,9 @@
     <title>$Title$</title>
   </head>
   <body>
+  code : <%=session.getAttribute("code") %>
+  <br/>
+  state: <%=session.getAttribute("state") %>
   $END$
   </body>
 </html>
