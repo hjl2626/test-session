600c07ff5e1f823551bc5e16c01aeefba4c2c47b
hjl <799728970@qq.com>
2017-04-23 20:47:57 +0800

    update code

--- src/main/java/com/hjl/controller/Index.java
+++ src/main/java/com/hjl/controller/Index.java
@@ -21,7 +21,7 @@ public class Index {
 	@RequestMapping("redirect")
 	public String test(HttpServletResponse response){
 		
-		return "redirect:123.57.238.125/session-test/test/login?code=123&state=456";
+		return "redirect:http://localhost:8080/test/login?code=123678&state=456789";
 	}
 	
 	
@@ -30,7 +30,7 @@ public class Index {
 		HttpSession session = request.getSession();
 		session.setAttribute("code" ,code);
 		session.setAttribute("state" , state);
-		return "redirect:index";
+		return "redirect:/index.jsp";
 	}
 	
 	
